5965fa9cf8a188c0b6fc4b041238df21ec446fb6
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-09-17 21:17:32 +0300

    add cors

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const visitRouter = require("./routes/visit-routes");
 
@@ -7,6 +8,7 @@ const PORT = 3002;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(visitRouter)
 
 mongoose
