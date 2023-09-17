3d2c10f95561465f4e83cb27e9c00d2a1228b4d7
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-09-17 19:41:21 +0300

    get data from db

--- server.js
+++ server.js
@@ -16,5 +16,25 @@ conectToDb((err) => {
     } else {
         console.log(`DB connection err ${err}`)
     }
+});
+
+app.get("/ip", (req, res) => {
+    const visits = [];
+
+    db
+        .collection("visits")
+        .find()
+        .forEach((visit) => visits.push(visit))
+        .then(() => {
+            res.status(200)
+                .json(visits)
+        })
+        .catch(() => {
+            res
+                .status(500)
+                .json({ error: "Somthing does wrong" })
+        }
+
+        )
 })
 
