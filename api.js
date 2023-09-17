29dac7d67a4d70372e687632d5f3827ed40eb813
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-09-17 20:09:05 +0300

    add dotenv

--- api.js
+++ api.js
@@ -1,16 +1,16 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const visitRouter = require("./routes/visit-routes");
 
 const PORT = 3002;
-const URL = "mongodb://0.0.0.0:27017/dbBlogPage";
 
 const app = express();
 app.use(express.json());
 app.use(visitRouter)
 
 mongoose
-    .connect(URL)
+    .connect(process.env.URL)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log(`DB connection error: ${err}`));
 
