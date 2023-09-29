92f492c46cf053e151e2970f00c306927fd2749d
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-09-29 21:24:16 +0300

    change time mode

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,20 +1,36 @@
-const Comment =require("../models/comment");
+const Comment = require("../models/comment");
 
 const handleError = (res, error) => {
     res.status(500).json({ error });
 };
 
 const addComment = (req, res) => {
-    console.log(req.body)
-    const comment = new Comment(req.body);
+
+    const currentDate = new Date();
+
+    // Добавляем 3 часа к текущей дате и времени
+    currentDate.setHours(currentDate.getHours() + 3);
+
+    // Преобразуем дату и время в строку в формате UTC
+    const gmtPlus3TimeString = currentDate.toUTCString();
+
+    const newRecord = {
+        name: req.body.name,
+        surname: req.body.surname,
+        comment: req.body.comment,
+        date: gmtPlus3TimeString,
+        ipAddress: req.ip || req.headers['x-forwarded-for']
+
+    }
+    const comment = new Comment(newRecord);
     comment
         .save()
-        .then((result) => {
+        .then(() => {
             // Отправляем пустой ответ с кодом 201 (Created)
             res.sendStatus(201);
         })
         .catch((err) => handleError(res, err));
-    
+
 }
 
 module.exports = { addComment };
